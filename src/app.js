106b2582d3c603d044e3534521d99644c0d96e9e
Segoakkaya <akkayasergen59@gmail.com>
2023-11-29 10:58:46 +0300

    animate update

--- src/app.js
+++ src/app.js
@@ -4,6 +4,5 @@ window.jQuery = $;
 jQuery = $;
     require("../node_modules/bootstrap/dist/js/bootstrap"),
     require("../node_modules/owl.carousel/dist/owl.carousel"),
-    
     require("../node_modules/aos/dist/aos"),
     require("./js/script");
--- src/js/script.js
+++ src/js/script.js
@@ -131,6 +131,7 @@
             });
 
         },
+
         init: function () {
             script.app.owlInit();
             script.app.mobilemenu();
@@ -212,7 +213,25 @@
             $(".burger").on("click",function(){
                 $(this).toggleClass("close");
             });
-            AOS.init();
+              
+        },
+        aos:function(){
+ 
+            var AOS = require('aos');
+            AOS.init({
+                disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+                startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+                initClassName: 'aos-init', // class applied after initialization
+                animatedClassName: 'aos-animate', // class applied on animation
+                useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+                disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+                debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+                throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+                delay: 50, // values from 0 to 3000, with step 50ms
+                duration: 1000, // values from 0 to 3000, with step 50ms
+                easing: 'ease-in-out', // default easing for AOS animations
+                mirror: true
+              });             
         },
         utility: {
             cookie: {
@@ -301,3 +320,9 @@ Script.runModule = function (module) {
             break;
     }
 }
+
+window.addEventListener('load', function() {
+    script.app.aos();
+ 
+  
+});
